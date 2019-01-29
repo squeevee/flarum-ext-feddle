e7e49f2c1c1b50b5fd1cb15d7753462fc8f149c7
Ellie Hawk <squeevee@yahoo.com>
2019-01-29 16:31:57 -0700

    dbConnectionParams fix

--- src/ActivityPub/ActivityPubFactory.php
+++ src/ActivityPub/ActivityPubFactory.php
@@ -20,16 +20,21 @@ class ActivityPubFactory
         $flarum_config = $this->app->make('flarum.config');
         $database_config = $flarum_config['database'];
 
+        if ($database_config['driver'] !== 'mysql') {
+            //unsupported
+            return null;
+        }
+
         $ap_config = ActivityPubConfig::createBuilder()
             ->setDbConnectionParams(array(
                 'driver' => 'pdo_mysql',
-                'user' => 'flarum',
-                'password' => 'flarumpass',
-                'host' => 'localhost',
-                'port' => 3306,
-                'dbname' => 'flarum'
+                'user' => $database_config['username'],
+                'password' => $database_config['password'],
+                'host' => $database_config['host'],
+                'port' => intval($database_config['port']),
+                'dbname' => $database_config['database']
             ))
-            ->setIsDevMode(true)
+            ->setIsDevMode($flarum_config['debug'])
             ->setMetadataMappings(array(
                 __DIR__ . '/../../resources/orm_mappings'
             ))
